b9d00dfaa6aa15351b872b543f3fc7531c5cfe83
Rony <ronyjohn9870@gmail.com>
2025-03-17 22:45:20 +0200

    initial commit

---
+++ src/assets/mock-posts.ts
@@ -0,0 +1,20 @@
+export const Posts = [
+  {
+    title: 'News Letter one',
+    date: 'Oct 09 2024',
+    textBody:
+      'It was originally taken from a Latin text written by a Roman Scholar, Sceptic and Philosopher by the name of Marcus Tullius Cicero, who influenced the Latin language greatly.',
+  },
+  {
+    title: 'Article Two',
+    date: 'Jun 15 2025',
+    textBody:
+      'The "filler" text we know today has been altered over the years (in fact "Lorem" is not actually a Latin word. It is suggested that the reason that the text starts with "Lorem" is because there was a page break spanning the word "Do-lorem". If you a re looking for a translation of the text, it is meaningless. The original text talks about the pain and love involved in the pursuit of pleasure or something like that.',
+  },
+  {
+    title: 'Post 3',
+    date: 'Aug 23 2023',
+    textBody:
+      'The reason we use Lorem Ipsum is simple. If we used real text, it would possibly distract from the DESIGN of a page (or indeed, might even be mistakenly inappropriate. Or if we used something like "Insert Text Here...", this would also distract from the design. Using Lorem Ipsum allows us to SEE the design without being distracted by readable or unrealistic text.',
+  },
+];
